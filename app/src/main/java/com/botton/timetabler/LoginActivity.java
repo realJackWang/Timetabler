ae0fb9e2572f4b3ff37bb8b2705346ba018090af
JackWang <544907049@qq.com>
2018-11-10 15:52:39 +0800

    Revert "Revert "键键上传云端了""
    This reverts commit b90373ffa9f3bc7506fd5c0eadefac5f8cd39a80.

---
+++ app/src/main/java/com/botton/timetabler/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.botton.timetabler;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.botton.timetabler.Activity.BtmBarActivity;
+
+public class LoginActivity extends AppCompatActivity {
+     String Id ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+        Button login = findViewById(R.id.login);
+        TextView studentId = findViewById(R.id.studentId);
+        Id = studentId.getText().toString();
+
+        //点击登陆按钮 就跳转到课程表主界面辣
+        login.setOnClickListener((view -> {
+            Intent intent = new Intent();
+            intent.setClass(LoginActivity.this, BtmBarActivity.class);
+            intent.putExtra("userId",Id);
+            LoginActivity.this.startActivity(intent);
+        }));
+        //************************//
+    }
+
+}
