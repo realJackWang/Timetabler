f396372ded4d5b1f3da1e3ae4b03f4f11c4376a6
JillXD <842667251@qq.com>
2018-11-21 08:43:25 +0800

    修改了部分按钮ui

---
+++ app/src/main/java/com/botton/timetabler/activity/TodoListActivity.java
@@ -0,0 +1,15 @@
+package com.botton.timetabler.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.botton.timetabler.R;
+
+public class TodoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+    }
+}
