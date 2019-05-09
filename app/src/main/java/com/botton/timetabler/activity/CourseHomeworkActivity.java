ba41112ffcc36aa948925b21758ab103b297b596
JillXD <842667251@qq.com>
2019-05-09 19:43:58 +0800

    发现界面改动

--- app/src/main/java/com/botton/timetabler/activity/CourseHomeworkActivity.java
+++ app/src/main/java/com/botton/timetabler/activity/CourseHomeworkActivity.java
@@ -2,14 +2,32 @@ package com.botton.timetabler.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.botton.timetabler.R;
 
 public class CourseHomeworkActivity extends AppCompatActivity {
 
+    Button hw_sta = findViewById(R.id.hw_new);
+    Button hw_all = findViewById(R.id.hw_all);
+    ListView lw = findViewById(R.id.listView);
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_homework);
+
+
+
+
     }
+
+
+
+
+
 }
